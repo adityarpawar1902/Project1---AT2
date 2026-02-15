a7bb34b5998c3b92f4ebda16b45b5dfc9356969e
theodorsommer <theodor.sommer2006@gmail.com>
2026-02-14 23:23:55 -0500

    Update synth_helpers.py

--- synth_helpers.py
+++ synth_helpers.py
@@ -11,6 +11,8 @@ def genSine(freq, dur, fs=44100, amp=1, phi=0):
     t = np.arange(0, dur, 1/fs)
     return amp * np.sin(2*np.pi * freq * t + phi)
 
+# push check
+
 def genSaw(freq, dur, fs=44100, amp=1, phi=0):
     saw = genSine(freq, dur, fs, amp, phi)
     nyquist = fs / 2
@@ -112,7 +114,77 @@ def adsr(data, attack, decay, sustain, release, fs=44100):
     The function should return a numpy array
     sig (numpy array) = the modified, enveloped signal
     """
-    sig = data
+    t = len(data)/fs
+
+    a_time = ((attack/100) * t) * 1000
+    d_time = ((decay/100) * t) * 1000
+    r_time = ((release/100) * t) * 1000
+
+    if a_time is None:
+        a_time = (1/5 * t)
+        print("Attack time not specified. a_time set to default of 1/5 of audio length.")
+
+    if d_time is None:
+        d_time = (1/5 * t)
+        if sustain != None:
+            print("Decay time not specified. d_time set to default of 1/5 of audio length.")
+
+    if r_time is None:
+        r_time = (1/5 * t)
+        print("Release time not specified. r_time set to default of 1/5 of audio length.")
+    elif r_time < 20 and not (t-0.02 <= a_time <= t):
+        r_time = 20
+        print("Release time must be at least 20ms. r_time set to 20ms.")
+    elif (t-0.02 <= a_time <= t):
+        r_time = t - a_time
+        print("Attack time goes to within 20ms of end of audio signal. Release time set to amount of remaining ms, or: ", r_time)
+
+    if a_time + d_time + r_time > t * 1000:
+        a_time = (1/5 * t * 1000)
+        d_time = (1/5 * t * 1000)
+        r_time = (1/5 * t * 1000)
+        print("Attack, decay and release time combine to a duration longer than input audio. Please input smaller values. Values set to defaults (each 1/5 of audio length).")
+
+    if sustain is None: 
+        a_samp = int(a_time * fs / 1000)
+        d_samp = 0
+        r_samp = int(r_time * fs / 1000)
+        s_samp = 0
+        zero_arr = np.zeros((len(data) - a_samp - d_samp - r_samp))
+        zero_samp =len(zero_arr)
+        env_samp = a_samp + d_samp + r_samp + s_samp + zero_samp #for error handling only
+        print("No sustain input. Sustain and decay parameters nullified, only attack and release applied. Amplitude of 0 applied to remainder of audio.")
+    else: 
+        a_samp = int(a_time * fs / 1000)
+        d_samp = int(d_time * fs / 1000)
+        r_samp = int(r_time * fs / 1000)
+        s_samp = int(len(data) - a_samp - d_samp - r_samp)
+        zero_arr = []
+        zero_samp = len(zero_arr)
+        env_samp = a_samp + d_samp + r_samp + s_samp + zero_samp #for error handling only
+
+    if env_samp != len(data) and s_lvl is None:
+       zero_arr = np.zeros(len(data) - a_samp - d_samp - r_samp + (len(data) - env_samp))
+       zero_samp = len(zero_arr)
+       env_samp = a_samp + d_samp + r_samp + s_samp + zero_samp
+    else: 
+       s_samp = s_samp + (len(data) - env_samp)
+       env_samp = a_samp + d_samp + r_samp + s_samp + zero_samp
+    #Compensates envelope length in case of length inconsistency in envelope array and original audio array caused by truncation by the int function.
+
+    if sustain is None:
+        a = np.linspace(0, 1, a_samp)
+        r = np.linspace(1, 0, r_samp)
+        z = np.full(len(zero_arr), 0)
+        env = np.concatenate([a,r,z])
+    else: 
+        a = np.linspace(0, 1, a_samp)
+        d = np.linspace(1, sustain, d_samp)
+        s = np.full(s_samp, sustain)
+        r = np.linspace(sustain, 0, r_samp)
+        env = np.concatenate([a,d,s,r])
+
+    sig = env * data
     return sig
 
 
@@ -134,6 +206,7 @@ def fm_synth(carrier_type, carrier_freq, mod_index, mod_ratio, dur, fs=44100, am
     The function should return a numpy array
     sig (numpy array) = frequency modulated signal
     """
+<<<<<<< HEAD
     carrier_type = str(carrier_type)
     carrier_freq = float(carrier_freq)
     mod_index = float(mod_index)
@@ -154,6 +227,9 @@ def fm_synth(carrier_type, carrier_freq, mod_index, mod_ratio, dur, fs=44100, am
             raise InvalidInputError('Modulator ratio must be greater than 0.')
     except InvalidInputError as e:
         return e
+=======
+    
+>>>>>>> main
 
 # TODO: Replace the code below with your implementation of a AM synthesis
 def am_synth(carrier_type, carrier_freq, mod_depth, mod_ratio, dur, fs=44100, amp=1, modulator_type='sine'):
@@ -218,6 +294,8 @@ def filter(data, type, cutoff_freq, fs=44100, order=5):
     nyq = 0.5 * fs
     normal_cuttoff = cutoff_freq / nyq
 
+    #AI SHIT: if band pass/band stop, cutoff_freq should be a list of two frequencies, and normal_cutoff should be a list of two values. Also, type_option should include bandpass and bandstop. Then, in the if statement, you would need to check if type is in the new type_option, and if type is bandpass or bandstop, then use the new normal_cutoff and btype in the butter function.
+    #should be an array rather than a float (cutoff for bandpass/bandstop 
     try:
         if (type in type_option) & (type(type) == str) & (cutoff_freq > 0) & (fs > 0) & (order > 0 and order <= 6):
            nyq = 0.5 * fs
